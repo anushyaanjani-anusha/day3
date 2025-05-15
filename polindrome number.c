ccbe5df524c7497533915a6ce7eae7a09b2d5baf
anushyaanjani-anusha <anushaapsm@gmail.com>
2025-05-15 14:59:07 +0530

    Create polindrome number.c

---
+++ polindrome number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int num,reversed=0,rem,original;
+printf("enter the number int");
+scanf("%d",&num);
+while(num!=0){
+    rem=num%10;
+    reversed=reversed*10+rem;
+    num=num/10;
+}
+if(num=original){
+    printf("the number is polidrome");
+}
+else;
+}
